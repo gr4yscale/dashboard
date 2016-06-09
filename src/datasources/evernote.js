b16b401de41cf5c8c1e9ef781acdb3e363e9f31b
Tyler Powers <gr4yscale@gmail.com>
2016-06-09 18:32:42 +0100

    Don't crash when we haven't authed to evernote yet

--- src/datasources/evernote.js
+++ src/datasources/evernote.js
@@ -20,9 +20,14 @@ export default class DataSourceEvernote {
 
       // for now I just care about a few specific notes from evernote, this datasource will just provide convenience to their content
       this.noteStore.getNote(this.accessToken, 'bbce1a9e-4d1d-4c86-a20a-4ebdba22ed96', true, false, false, false, (err, note) => {
-        this.data['scratchpadNoteContent'] = note.content
-        console.log('* Fetched Evernote data')
-        resolve(this.data)
+        if (err) {
+          console.log('Evernote datasource encountered an error')
+          console.log(err)
+        } else {
+          this.data['scratchpadNoteContent'] = note.content
+          console.log('* Fetched Evernote data')
+          resolve(this.data)  
+        }
       })
     })
   }
